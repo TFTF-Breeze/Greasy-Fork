e47b1b5d49a2b03ddb35cb375b1b947b3c6fbad4
TFTF-Breeze <1657789778@qq.com>
2025-02-12 19:15:05 +0800

    整理目录

---
+++ 自动刷课/国家中小学智慧教育平台.js
@@ -0,0 +1,58 @@
+// ==UserScript==
+// @name         国家中小学智慧教育平台
+// @namespace    http://tampermonkey.net/
+// @version      2025-02-08
+// @description  try to take over the world!
+// @author       You
+// @match        https://basic.smartedu.cn/teacherTraining/*
+// @icon         https://basic.smartedu.cn/img/logo-icon.afa526cf.png
+// @grant        none
+// ==/UserScript==
+
+// 定义一个函数，输出当前时间
+function printCurrentTime() {
+    let duration = document.getElementsByTagName('video')[0].duration;//视频总时长
+    let current = document.getElementsByTagName('video')[0].currentTime;//视频当前播放时长
+    if(duration-1 <= current){
+        //打开所有目录
+        let course_top = document.querySelectorAll(".fish-collapse-header");
+        for(let i=0;i<=course_top.length-1;i++){
+            if(course_top[i].querySelector("div > i > svg").style[0] == undefined){
+                course_top[i].click();
+            }
+        }
+
+        let course = document.querySelectorAll(".resource-item > div:nth-child(2) > div");//课程目录
+        for(let i=0;i<=course.length-1;i++){
+            if(course[i].querySelector("div") != null){
+                if(i < course.length-1){
+                    course[i+1].click();
+                }else{
+                    alert('视频已经全部播放完毕！！！');
+                }
+            }
+        }
+    }else{
+        //不够播放时间继续播放
+        let play = document.querySelector(".vjs-icon-placeholder")//点击播放;
+        if(play != null){
+            play.click();
+            console.log("已点击播放！！！");
+        }
+    }
+
+
+    let Learning_detection = document.querySelector(".fish-modal-confirm-btns > button");//学习检测
+    if(Learning_detection != null){
+        Learning_detection.click(); //确认学习检测
+        console.log("已确认学习检测！！！");
+    }
+}
+
+(function() {
+    'use strict';
+
+    // 每5秒运行一次printCurrentTime函数
+    setInterval(printCurrentTime, 5000);
+
+})();
